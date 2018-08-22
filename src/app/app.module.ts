4d689844612f7b64ec65230b1936d49b9cca9bf9
Virros <virros.work@gmail.com>
2018-08-22 07:56:21 +0400

    First commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,63 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {RouterModule} from '@angular/router';
+import { HttpClientModule }    from '@angular/common/http';
+
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {
+  MatTabsModule,
+  MatToolbarModule,
+  MatIconModule,
+  MatSidenavModule,
+  MatButtonModule,
+  MatCardModule,
+  MatInputModule,
+  MatSelectModule,
+  MatGridListModule,
+  MatListModule
+} from '@angular/material';
+import { BarMenuComponent } from './bar-menu/bar-menu.component';
+import { HomePageComponent } from './home-page/home-page.component';
+import { AutorizationComponent } from './autorization/autorization.component';
+import { ProfileComponent } from './profile/profile.component';
+
+const routs = [
+  {path: '', component: HomePageComponent},
+  {path: 'autorization', component: AutorizationComponent},
+  {path: 'my_profile', component: ProfileComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BarMenuComponent,
+    HomePageComponent,
+    AutorizationComponent,
+    ProfileComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule,
+    RouterModule.forRoot(routs),
+    BrowserAnimationsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    MatTabsModule,
+    MatToolbarModule,
+    MatIconModule,
+    MatSidenavModule,
+    MatButtonModule,
+    MatCardModule,
+    MatInputModule,
+    MatSelectModule,
+    MatGridListModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
